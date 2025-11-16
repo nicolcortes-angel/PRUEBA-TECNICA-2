c98edb133d39e8d65d6eaad2d6fd1305e37a6c30
nicolcortes-angel <nicolmurciacortes7@gmail.com>
2025-11-16 17:23:18 -0500

    actualizacion

--- src/routes/login.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login } from "../services/login.js";
-
-
-export const loginRouter = express.Router();
-
-//3. Ruta para el POST 
-loginRouter.post("/", login);
-
